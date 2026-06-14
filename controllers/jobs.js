f94b8be7bd1daa9fbb53cf11823383a0ba6a50e5
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-06-14 13:13:14 +0600

    aaded recruiter job find route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,6 +2,10 @@ const { getCollection } = require("../db/dbConnect.js");
 
 const createNewJob = async (req, res)=>{
     const jobData = req.body;
+    const newJob = {
+        ...jobData,
+        createdAt: new Date()
+    }
     const jobCollection = await getCollection("jobs");
     const result = await jobCollection.insertOne(jobData);
     res.json(result);
@@ -18,5 +22,19 @@ const getCompanyJobs = async (req, res)=>{
     const result = await jobCollection.find(query).toArray();
      res.json(result);
 }
+const getRecruiterJobs = async (req, res)=>{
+    try {
+        const query = {};
+        if(req.query.recruiterId){
+            query.recruiterId = req.query.recruiterId
+        }
+        const jobsCollection = await getCollection("jobs");
+        const result = await jobsCollection.find(query).toArray();
+        res.json(result);
 
-module.exports = {createNewJob, getCompanyJobs}
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {createNewJob, getCompanyJobs, getRecruiterJobs}
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createNewJob, getCompanyJobs } = require("../controllers/jobs.js");
+const { createNewJob, getCompanyJobs, getRecruiterJobs } = require("../controllers/jobs.js");
 const { createCompany, getRecruiterCompany } = require("../controllers/companys.js");
 const appRouter = express.Router();
 appRouter.post("/create-new-job", createNewJob);
 appRouter.get("/companyjob", getCompanyJobs);
 appRouter.post("/createcompany", createCompany);
 appRouter.get("/recruiter-company", getRecruiterCompany);
+appRouter.get("/get-recruiter-jobs", getRecruiterJobs);
 module.exports = { appRouter }
